7c7c1572cea4ea07c9cad19305dccfd778680879
MPASTEAU <mathis.pasteau@eficia.com>
2024-08-06 16:15:46 +0200

    [DEV] - First commit

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_get_time():
+    response = client.get("/time")
+    assert response.status_code == 200
+    assert "time" in response.json()
