9be1473e074ffdabe358cd109d502941873f4db5
MPASTEAU <93317245+MPASTEAU@users.noreply.github.com>
2024-08-08 17:48:38 +0200

    [DEV] - Remove routes.

--- app/main.py
+++ app/main.py
@@ -7,13 +7,3 @@ app = FastAPI()
 @app.get("/time")
 def get_time():
     return {"time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
-
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
-
-@app.get("/version")
-def version():
-    return {"version": "0.1.0"}
